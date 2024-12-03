15a4afa75fd4f86f88cfa9a70c63677846e76b2f
zhiwang24 <zhiwang224@gmail.com>
2024-12-03 01:35:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from 'react';
 import './App.css';
 import Background from './components/Background';
 import AudioMixer from './components/AudioMashup';
+// Disable Right-Click Context Menu
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 
+// Detect and Block Debugging
+(function detectDebugging() {
+  const blockDebugging = () => {
+    console.clear();
+    console.error("Debugging is disabled!");
+  };
+
+  const checkDebugger = setInterval(() => {
+    if (window.console && (console.log || console.debug)) {
+      console.log = console.debug = blockDebugging;
+    }
+  }, 1000);
+
+  window.addEventListener("beforeunload", () => clearInterval(checkDebugger));
+})();
 function App() {
   return (
     <div className="App">
